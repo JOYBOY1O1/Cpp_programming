182d401f61ce3bb057ba49859ab91e93a0cfcd65
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-04-17 14:14:59 +0530

    Create 25_cast_operator.cpp

---
+++ Basic/25_cast_operator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char ch = 'a';
+    int c = int(ch); // c = 97 which is ascii value of a = 97
+
+    cout<<c<<"\n"<<endl;
+
+    int n=(1,2,3,4);
+
+    cout<<"Using comma operator the value of n in (1,2,3,4) will be = "<<n;
+    
+}
