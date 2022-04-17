963b72bdde0e0f0c5e3b8d266cf497fefcec4ebf
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-04-17 14:14:16 +0530

    Create 13_pointers_arrays.cpp

---
+++ Basic/13_pointers_arrays.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int marks[] = {23, 45, 56, 89};
+
+    int *p = marks;
+
+    cout << "The value of marks[0]   *p   is = " << *p << endl;
+    cout << "The value of marks[1] *(p+1) is = " << *(p+1) << endl;
+    cout << "The value of marks[2] *(p+2) is = " << *(p+2) << endl;
+    cout << "The value of marks[3] *(p+3) is = " << *(p+3) << endl;
+
+    cout<<*(p++)<<endl;
+    cout<<*(++p)<<endl;
+ //   cout<<*p<<endl;
+
+    return 0;
+}
