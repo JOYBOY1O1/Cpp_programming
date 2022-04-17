ea740a07b3bb74339db23af0ae6c64bfd2ed87df
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-04-17 14:14:02 +0530

    Create 8_cmath.cpp

---
+++ Basic/8_cmath.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    cout << 10 / 3.0 << endl;
+
+    cout << pow(3, 3) << endl;
+
+    cout << sqrt(36) << endl;
+
+    cout << round(4.5) << endl;
+
+    cout << ceil(4.1) << endl;
+
+    cout << floor(4.9) << endl;
+
+    cout << fmax(5,10) << endl;
+
+    cout << fmin(5,10) << endl;
+
+}
