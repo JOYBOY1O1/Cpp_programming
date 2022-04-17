bc308bad8b025f1a96140c6055d96f7c30c55d87
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-04-17 14:13:36 +0530

    Create 1_if_else.cpp

---
+++ Basic/1_if_else.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+
+    if(a>b)
+    {
+        if(a>c)
+        {
+            cout<<a<<endl;
+        }
+        else cout<<c<<endl;
+    }
+    else
+    {
+        if(b>c)
+        {
+            cout<<b<<endl;
+        }
+        else cout<<c<<endl;
+    }
+
+    cout<<"Enter a number to check Even or ODD = ";
+    int n;
+    cin>>n;
+    if(n%2==0)
+    {
+        cout<<n<<" is even number"<<endl;
+    }
+    else cout<<n<<" is odd number"<<endl;
+
+    return 0;
+}
