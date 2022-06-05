75414c15d97bc248a29b2454fa9d44489a4cd672
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-06-06 01:06:59 +0530

    Update TowerOfHanoi.cpp

--- DSA_DAA/TowerOfHanoi.cpp
+++ DSA_DAA/TowerOfHanoi.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int TOH(int n, char from, char to, char aux)
+void TOH(int n, char from, char to, char aux)
 {
     if (n == 1)
-        return n;
+        return;
     TOH(n - 1, from, aux, to);
     cout << "Move Disk " << n << " from " << from << " to " << to <<endl;
     TOH(n - 1, aux, to, from);
