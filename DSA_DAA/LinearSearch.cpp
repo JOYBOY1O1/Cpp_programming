671a384717453d2c16c83020e9ec68b58843f1e8
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-06-06 00:47:16 +0530

    Update LinearSearch.cpp

--- DSA_DAA/LinearSearch.cpp
+++ DSA_DAA/LinearSearch.cpp
@@ -14,7 +14,7 @@ int main()
 {
     int A[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     int size = sizeof(A) / sizeof(A[0]);
-    int key = 5;
+    int key = 9;
     int result = LinearSearch(A, size, key);
-    cout << result;
+    cout << "Index of "<<key<<" is "<<result;
 }
